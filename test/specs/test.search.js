8a31593ee92409d9464c2a1620f2696454d0861c
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2024-02-04 22:35:08 +0200

    removed unneeded import

--- test/specs/test.search.js
+++ test/specs/test.search.js
@@ -2,7 +2,6 @@ const LoginPage = require('../pageobjects/login.page')
 const credentials = require('../../wdio.conf')
 const BoardsPage = require('../pageobjects/boards.page')
 const {BOARD_INITIAL,USER_NAME} = require('../pageobjects/config')
-const fs = require('fs');
 
 describe('Verify Search from main menu', () => {
 
@@ -22,8 +21,8 @@ describe('Verify Search from main menu', () => {
         timeout: 10000,
         timeoutMsg: 'page not fully loaded after 10 sec'
         })
-         await BoardsPage.mainMenu.menuItem('searchInput').setValue(BOARD_INITIAL);
-         await BoardsPage.mainMenu.menuItem('searchResult').click();
-         await expect(BoardsPage.item('boardName')).toHaveText(BOARD_INITIAL);
+        await BoardsPage.mainMenu.menuItem('searchInput').setValue(BOARD_INITIAL);
+        await BoardsPage.mainMenu.menuItem('searchResult').click();
+        await expect(BoardsPage.item('boardName')).toHaveText(BOARD_INITIAL);
     })
 })
