9cb2b7bcd37c25d81f80d447139d5ae2f0c033ef
ZhytnkovskaIryna <iryna.zhytnikovska@gmail.com>
2024-03-24 15:50:20 +0200

    Update api test work witt .env file

--- API/src/api_client/api_client.js
+++ API/src/api_client/api_client.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 const { BASE_URL_API, BASE_URL_CONTENT } = require("./api_data");
-const jsonData = require("../../env.json");
+require("dotenv").config();
 
 const instanceAPI = axios.create({
   baseURL: BASE_URL_API,
   timeout: 5000,
   headers: {
-    Authorization: `Bearer ${jsonData.TOKEN}`,
+    Authorization: `Bearer ${process.env.TOKEN}`,
   },
 });
 
@@ -14,7 +14,7 @@ const instanceContent = axios.create({
   baseURL: BASE_URL_CONTENT,
   timeout: 5000,
   headers: {
-    Authorization: `Bearer ${jsonData.TOKEN}`,
+    Authorization: `Bearer ${process.env.TOKEN}`,
   },
 });
 
